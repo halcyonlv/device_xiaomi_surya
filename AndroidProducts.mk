16d84c31e4ce093a8eb28058b1ca4dd07e530b73
shoya0x00 <shoya0x00@gmail.com>
2024-10-05 02:10:09 +0600

    surya: Bringup Halcyon

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_surya.mk
+    $(LOCAL_DIR)/halcyon_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_surya-user \
-    lineage_surya-userdebug \
-    lineage_surya-eng
+    halcyon_surya-user \
+    halcyon_surya-userdebug \
+    halcyon_surya-eng
