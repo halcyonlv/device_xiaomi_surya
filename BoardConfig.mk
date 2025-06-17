5305efa808e5168b1129a8ef4611010f7bb98ce7
Arian <arian.kulmer@web.de>
2025-06-17 17:56:44 +0200

    Revert "surya: Include vendor/debugfs.config"
    We disable debug fs in the defconfigs directly since
    I01a4dd199a357dd85838c071020bf966079a2092.
    
    This reverts commit f0db14d1e37dc13dcc23d5fb90ba87700c1c4736.
    
    Change-Id: Ifcf9dd9d7a48ee93999c783f0f361e75bb97e364

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_MKBOOTIMG_ARGS := --header_version 2
 
-TARGET_KERNEL_CONFIG := surya_defconfig vendor/debugfs.config
+TARGET_KERNEL_CONFIG := surya_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 
 # Media
