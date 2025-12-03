fd0cd890a565e9e26331409160adf7f30d918676
Raya Mahardika <rayapkun@hlcyn.co>
2025-12-03 10:45:16 +0700

    surya: Move Aperture overlay to RRO
    Change-Id: Ifd4e68b23fbd11e365fc9d90c6c7dbf8d4470d80

--- device.mk
+++ device.mk
@@ -75,12 +75,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_surya/com.nxp.mifare.xml
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aperture
-
-PRODUCT_ENFORCE_RRO_TARGETS += *
-
 PRODUCT_PACKAGES += \
+    J20CAperture \
     J20CFrameworks \
     J20CSystemUI \
     KarnaFrameworks \
