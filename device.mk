4e0ad98d4b5aed54dcd1c31efb463790936ec042
basamaryan <basam.aryan@gmail.com>
2025-12-13 18:02:38 -0800

    surya: overlay: Reduce blur radius
    Change-Id: Id6339df82156099ca14da684f236931b3ecf51ce

--- device.mk
+++ device.mk
@@ -279,6 +279,7 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_PACKAGES += \
     J20CCarrierConfig \
     J20CFrameworks \
+    J20CLauncher3 \
     J20CSettings \
     J20CSettingsProvider \
     J20CSystemUI \
