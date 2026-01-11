40c6ec9f850adb6d9ad28876b06f59d736c44056
Raya Mahardika <rayapkun@hlcyn.co>
2026-01-11 13:15:57 +0000

    surya: Symlink audio configs for sku_surya
    Change-Id: Ibf42a4f3ab827913202715193c5101cac41c7ebc

--- device.mk
+++ device.mk
@@ -24,6 +24,9 @@ $(call inherit-product-if-exists, device/xiaomi/miuicamera-surya/device.mk)
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Audio
+PRODUCT_PACKAGES += \
+    sku_surya_audio_symlink
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
