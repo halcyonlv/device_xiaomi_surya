e418ba454040ade7107f1e19c3f544428ca37a5c
Raya Mahardika <rayapkun@hlcyn.co>
2026-03-10 04:12:29 +0000

    surya: Update Display Stack from AOSPA
    Change-Id: I8999f1b79aef4e47e5cf1b3d4faf024495fc1aec

--- extract-files.py
+++ extract-files.py
@@ -51,8 +51,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libui_shim.so'),
     'vendor/lib64/libgoodixhwfingerprint.so': blob_fixup()
         .replace_needed('libvendor.goodix.hardware.biometrics.fingerprint@2.1.so', 'vendor.goodix.hardware.biometrics.fingerprint@2.1.so'),
-    'vendor/lib64/libdpps.so': blob_fixup()
-        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     ('vendor/lib64/libalAILDC.so', 'vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
