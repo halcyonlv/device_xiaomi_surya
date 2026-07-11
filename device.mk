5358b653f99daaf0087a2888bb56416248136f3e
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-11 18:16:27 +0000

    surya: Stop disabling configstore
    * As of CP2A it's just dead and gone.
    
    Change-Id: Ie73368e5e3e1e3295f0cdf47540e4b8825440f7b
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -122,9 +122,6 @@ PRODUCT_PACKAGES += \
     gralloc.qcom \
     hwcomposer.qcom
 
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
